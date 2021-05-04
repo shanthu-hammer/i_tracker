5f8c20a27ab7934a4c1458598f63e886affcf3b8
santhu-hammer <ssanthujan@yahoo.com>
2021-05-05 03:10:18 +0530

    investment form (UI)

--- lib/screens/investment.dart
+++ lib/screens/investment.dart
@@ -18,68 +18,70 @@ class _InvestmentState extends State<Investment> {
         ),
       ),
       backgroundColor: ThemeColor.background,
-      body: Padding(
-        padding: EdgeInsets.all(30),
-        child: Column(
-          //Name
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Name',
-                  hintText: 'Enter the Name of the investment'),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            //Description
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Description',
-                  hintText: '(optional)'),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Total  amount',
-                  hintText: 'Enter the total invested amount'),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Date',
-                  hintText: 'Enter the date of investment'),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: ThemeColor.button,
-                  borderRadius: BorderRadius.circular(20)),
-              child: FlatButton(
-                onPressed: () {
-                  //Navigator.of(context).pop();
-                  Navigator.pushNamed(context, '/dashboard');
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            //Name
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter the Name of the investment'),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              //Description
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Description',
+                    hintText: '(optional)'),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Total  amount',
+                    hintText: 'Enter the total invested amount'),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Date',
+                    hintText: 'Enter the date of investment'),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: ThemeColor.button,
+                    borderRadius: BorderRadius.circular(20)),
+                child: FlatButton(
+                  onPressed: () {
+                    //Navigator.of(context).pop();
+                    Navigator.pushNamed(context, '/dashboard');
 
-                  //ADD Route here
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(color: ThemeColor.background),
+                    //ADD Route here
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: ThemeColor.background),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

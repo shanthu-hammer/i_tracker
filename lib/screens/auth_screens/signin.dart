0a032c64f287c7648314bfe862077c84a6139234
Nikhil Khandelwal <nk2482@gmail.com>
2021-05-10 22:12:33 +0530

    added loginscreen

--- lib/screens/auth_screens/signin.dart
+++ lib/screens/auth_screens/signin.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:i_tracker/components/custom_appbar.dart';
-
+//TODO id , password
 class SignIn extends StatefulWidget {
   @override
   _SignInState createState() => _SignInState();
@@ -13,6 +13,76 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       //MyAppbar can take in the title from a Text Widget
       appBar: MyAppBar(title: Text('Login Page')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('asset/images/login.png')),
+              ),
+            ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
+               print("inside forget password") ;//TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+                onPressed: () {
+
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 130,
+            ),
+            Text('New User? Create Account')
+          ],
+        ),
+      ),
       //TODO "do the editing here"
     );
   }
--- lib/screens/auth_screens/signup.dart
+++ lib/screens/auth_screens/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:i_tracker/components/custom_appbar.dart';
+//TODOemail,password*2, first name lastname.
 class SignUp extends StatefulWidget {
   @override
   _SignUpState createState() => _SignUpState();
@@ -8,7 +9,7 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(appBar: MyAppBar(title: Text("Registering",))
 
         //TODO "do the editing here"
 
